05be8f11be12d415d754541bc57be70d9acbfb5b
Choy Dian Chun <choydianchun@gmail.com>
2026-03-09 17:00:29 +0800

    add rum

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,23 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
+import { datafluxRum } from '@cloudcare/browser-rum';
+
+datafluxRum.init({
+    applicationId: 'dian_rum',
+    site: 'https://id1-rum-openway.truewatch.com',
+    clientToken: '2c84ee66383f4cb781e51c89533fbff6',
+    env: 'production',
+    version: '1.0.0',
+    service: 'browser',
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 100,
+    compressIntakeRequests: true,
+    trackUserInteractions: true,
+});
+
+datafluxRum.startSessionReplayRecording()
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
