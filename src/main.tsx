01cde0a550bb06a3350e0bf75fdee2926d129dd5
Choy Dian Chun <choydianchun@gmail.com>
2026-03-09 17:26:41 +0800

    try again

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import './styles.css'
 import { datafluxRum } from '@cloudcare/browser-rum';
 
 datafluxRum.init({
-    applicationId: 'dian_rum',
+    applicationId: 'dian_rum_test',
     site: 'https://id1-rum-openway.truewatch.com',
-    clientToken: '2c84ee66383f4cb781e51c89533fbff6',
+    clientToken: '007419b3038445b9b0d909592f6ec82c',
     env: 'production',
     version: '1.0.0',
     service: 'browser',
@@ -16,12 +16,9 @@ datafluxRum.init({
     sessionReplaySampleRate: 100,
     compressIntakeRequests: true,
     trackUserInteractions: true,
-    // traceType: 'ddtrace', // Not required, default to ddtrace. Currently, it supports 6 types: ddtrace, zipkin, skywalking_v3, jaeger, zipkin_single_header and w3c_traceparent.
     allowedTracingOrigins: ['https://dian.hicloud.guru', 'https://react-rum-5529d.sevalla.page'],  // Not required; allow all requests to be injected into the header required by the trace collector. It can be the origin of the request or it can be regular.
 });
-
 datafluxRum.startSessionReplayRecording()
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
